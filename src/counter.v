7ca40277689e3af31a0aed22d9464fd70f527275
152 <152>
2018-05-09 11:48:12 -0700

    clock divider, counter started

---
+++ src/counter.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:40:44 05/09/2018 
+// Design Name: 
+// Module Name:    counter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module counter(
+	input clk,
+	output reg [6:0]  min,
+	output reg [5:0]  sec,
+	output reg [12:0] totalsec // max seconds is 5999
+);
+
+
+endmodule

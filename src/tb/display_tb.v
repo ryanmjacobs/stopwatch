ae6e010762c4dc84e3937be81ef2b1a2c3bead57
Ryan Jacobs <ryan@rmj.us>
2018-05-14 07:53:20 -0700

    kludge it so the testbench compiles

--- src/tb/display_tb.v
+++ src/tb/display_tb.v
@@ -1,8 +1,13 @@
 module display_tb;
     reg clk = 0;
+    reg [26:0] out1 = 0;
     reg [12:0] count = 119;
 
-    display display(clk, count);
+    wire [4:0] min_l = 0;
+    wire [4:0] min_r = 0;
+    wire [4:0] sec_l = 0;
+    wire [4:0] sec_r = 0;
+    display display(clk, out1, adj, min_l, min_r, sec_l, sec_r);
 
     initial begin
         $display("--- display tb ---");

5d298449e1f9ece0946a4cbb7d25917331ab4655
Ryan Jacobs <ryan@rmj.us>
2018-05-14 06:31:21 -0700

    segment with test

--- src/display.v
+++ src/display.v
@@ -1,31 +1,50 @@
 // count is measured in seconds
-module display(input clk, input [12:0] count);
+module display(input clk, input [12:0] count,
+    output [3:0] d0,
+    output [3:0] d1,
+    output [3:0] d2,
+    output [3:0] d3,
+    output [3:0] s0,
+    output [3:0] s1,
+    output [3:0] s2,
+    output [3:0] s3
+);
     // partitioned values
     reg [6:0] sec;
     reg [6:0] min;
-    reg [4:0] sec_l; reg [4:0] sec_r;
-    reg [4:0] min_l; reg [4:0] min_r;
+    reg [4:0] digits [0:3];
+
+    assign d0 = digits[0];
+    assign d1 = digits[1];
+    assign d2 = digits[2];
+    assign d3 = digits[3];
+
+    assign s0 = segments[0];
+    assign s1 = segments[1];
+    assign s2 = segments[2];
+    assign s3 = segments[3];
 
     always @(count) begin
         sec = count % 60;
         min = (count - sec) / 60;
 
-        // calculate the values of the left and
-        // right digits for seconds and minutes
-        sec_r = sec % 10;
-        sec_l = (sec - sec_r) / 10;
+        // calculate the values of the left
+        // and right digits for seconds
+        digits[3] = sec % 10;
+        digits[2] = (sec - digits[3]) / 10;
 
         // ditto
-        min_r = min % 10;
-        min_l = (min - min_r) / 10;
+        digits[1] = min % 10;
+        digits[0] = (min - digits[1]) / 10;
     end
 
     // calculate segment values
+    // in this order: min_l, min_r, sec_l, sec_r
+    genvar i;
     wire [3:0] segments [0:3];
-    segment segment0(min_l, segments[0]); // min left
-    segment segment1(min_r, segments[1]); // min right
-    segment segment2(sec_l, segments[2]); // sec left
-    segment segment3(sec_r, segments[3]); // sec right
+    for (i = 0; i < 4; i=i+1) begin
+        segment segment0(digits[i], segments[i]);
+    end
 
     // selected panel to render
     reg [3:0] panel;
@@ -41,17 +60,18 @@ module segment(value, storage);
 
     // lookup bit-value for 7-segment digit
     always @(value) begin
+        $display(value);
         case (value)
-            4'd0: storage = 7'b1000000;
-            4'd1: storage = 7'b1111001;
-            4'd2: storage = 7'b0100100;
-            4'd3: storage = 7'b0110000;
-            4'd4: storage = 7'b0011001;
-            4'd5: storage = 7'b0010010;
-            4'd6: storage = 7'b0000010;
-            4'd7: storage = 7'b1111000;
-            4'd8: storage = 7'b0000000;
-            4'd9: storage = 7'b0010000;
+            0: storage = 7'b1000000;
+            1: storage = 7'b1111001;
+            2: storage = 7'b0100100;
+            3: storage = 7'b0110000;
+            4: storage = 7'b0011001;
+            5: storage = 7'b0010010;
+            6: storage = 7'b0000010;
+            7: storage = 7'b1111000;
+            8: storage = 7'b0000000;
+            9: storage = 7'b0010000;
         endcase
     end
 endmodule
--- src/tb/display_tb.v
+++ src/tb/display_tb.v
@@ -2,10 +2,23 @@ module display_tb;
     reg clk = 0;
     reg [12:0] count = 119;
 
-    display display(clk, count);
+    wire [3:0] d0;
+    wire [3:0] d1;
+    wire [3:0] d2;
+    wire [3:0] d3;
+    wire [3:0] s0;
+    wire [3:0] s1;
+    wire [3:0] s2;
+    wire [3:0] s3;
+
+    display display(clk, count,
+        d0, d1, d2, d3,
+        s0, s1, s2, s3);
 
     initial begin
         $display("--- display tb ---");
+        #5 $display(count, d0, d1, d2, d3);
+        #5 $display(count, s0, s1, s2, s3);
         #100 $finish;
     end
 

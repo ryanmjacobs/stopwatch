a951166cebf6e2913d9bc5c732753cdf009b3e45
Ryan Jacobs <ryan@rmj.us>
2018-05-14 03:56:59 -0700

    begin linking modules

--- src/display.v
+++ src/display.v
@@ -9,17 +9,17 @@ module display(seconds, seg, an);
    
     assign an = panel;
     always @(num) begin
-   	case (num)
-   		4'd0: seg = 7'b1000000;
-   		4'd1: seg = 7'b1111001;
-   		4'd2: seg = 7'b0100100;
-   		4'd3: seg = 7'b0110000;
-   		4'd4: seg = 7'b0011001;
-   		4'd5: seg = 7'b0010010;
-   		4'd6: seg = 7'b0000010;
-   		4'd7: seg = 7'b1111000;
-   		4'd8: seg = 7'b0000000;
-   		4'd9: seg = 7'b0010000;
-   	endcase
+        case (num)
+            4'd0: seg = 7'b1000000;
+            4'd1: seg = 7'b1111001;
+            4'd2: seg = 7'b0100100;
+            4'd3: seg = 7'b0110000;
+            4'd4: seg = 7'b0011001;
+            4'd5: seg = 7'b0010010;
+            4'd6: seg = 7'b0000010;
+            4'd7: seg = 7'b1111000;
+            4'd8: seg = 7'b0000000;
+            4'd9: seg = 7'b0010000;
+        endcase
     end
 endmodule
--- src/top.v
+++ src/top.v
@@ -1,4 +1,29 @@
 module top;
-	module clkdiv();
-	module display();
+    reg rst = 0;
+	wire [26:0] out1;    // 1Hz Clock
+	wire [25:0] out2;    // 2Hz Clock
+	wire [17:0] out7seg; // 380Hz Clock
+	wire [25:0] outadj;  // 5Hz Clock
+    clkdiv clkdiv(clk, rst, out1, out2, out7seg, outadj);
+
+    // two control buttons
+    wire btn_reset;
+    wire btn_set_pause;
+    debounce db1(clk, btnRight,  btn_reset);
+    debounce db2(clk, btnCenter, btn_set_pause);
+    
+    // slider switches
+  //reg sel = sw[1:0];
+  //reg num = sw[7:4];
+  //reg adj = sw[8];
+    
+    // stopwatch count measured in seconds
+    reg count;
+
+
+    always @(posedge clk) begin
+    end
+    
+  //reg seg, an;
+  //display display(count, seg, an):
 endmodule

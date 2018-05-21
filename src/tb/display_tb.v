5d80b20cadcf4c42bbf93aa942041497735a173f
Ryan Jacobs <ryan@rmj.us>
2018-05-21 11:26:07 -0700

    display testbench

--- src/tb/display_tb.v
+++ src/tb/display_tb.v
@@ -1,17 +1,30 @@
 module display_tb;
     reg clk = 0;
-    reg [26:0] out1 = 0;
-    reg [12:0] count = 119;
+    wire [26:0] out1;    // 1Hz Clock
+    wire [25:0] out2;    // 2Hz Clock
+    wire [17:0] out7seg; // 380Hz Clock
+    wire [25:0] outadj;  // 5Hz Clock
+    clkdiv clkdiv(clk, btn_reset, out1, out2, out7seg, outadj);
 
-    wire [4:0] min_l = 0;
-    wire [4:0] min_r = 0;
-    wire [4:0] sec_l = 0;
-    wire [4:0] sec_r = 0;
-    display display(clk, out1, adj, min_l, min_r, sec_l, sec_r);
+    reg adj = 0;
+    reg [2:0] adj_sel = 0;
+
+	wire [6:0] seg;
+	wire [3:0] an;
+    wire [6:0] Led;
+	 
+	reg [4:0] min_l = 5;
+    reg [4:0] min_r = 6;
+    reg [4:0] sec_l = 7;
+    reg [4:0] sec_r = 8;
+    display display(clk, out7seg, outadj,
+                    adj_sel, adj, seg, an, Led,
+                    min_l, min_r, sec_l, sec_r);
 
     initial begin
         $display("--- display tb ---");
-        #20 $finish;
+        $monitor("seg=%b panel=%b", seg, an);
+        #1e7 $finish;
     end
 
     always begin
